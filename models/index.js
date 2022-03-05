109656c4e39b6e49abe565f5eb2596dc433299e5
Tinastouch <tinastouch@gmail.com>
2022-03-05 11:43:45 -0500

    models complete

--- models/index.js
+++ models/index.js
@@ -2,20 +2,35 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongTo(User, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-});
-
-Post.HasMany(Comment, {
-    foreignKey: 'postId',
-    onDelete: 'CASCADE'
-});
-
-Post.belongTo(User, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-});
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+  
 
 module.exports = {
     User,
